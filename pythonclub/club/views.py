3e7665e848a3c624a638c6c7534a2a0f02b891fd
Hanan Osman <hanan.osman@seattlecolleges.edu>
2021-05-14 23:12:44 -0700

    second assignment 5 commit

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def meeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meeting.html', {'meeting_list' : meeting_list})
