4cd90feb9384a34d3cffc62939cc1a9a2a29c609
Hanan Osman <hanan.osman@seattlecolleges.edu>
2021-05-21 22:32:18 -0700

    assignment 6 commit

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('resource/', views.resource, name='resource')
+    path('resource/', views.resource, name='resource'),
+    path('meeting/', views.meeting, name='meeting'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='detail'),
 ]
